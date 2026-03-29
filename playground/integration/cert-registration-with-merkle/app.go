c79e0c67d4f1359d8da126ad7898f59fd550675f
ajablonsk1 <adrian.tomasz.jablonski@gmail.com>
2026-03-29 22:07:16 +0200

    add 'cert-registration-with-merkle' playground mini project

---
+++ playground/integration/cert-registration-with-merkle/app.go
@@ -0,0 +1,251 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"crypto/ed25519"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"slices"
+	"strings"
+
+	merkle "local/merkle-trees"
+
+	abcitypes "github.com/cometbft/cometbft/abci/types"
+)
+
+type PKIApplication struct {
+	pkiState      *PKIState
+	tree          *merkle.MerkleTree
+	sortedDomains []string
+}
+
+type PKIState struct {
+	Certs map[string]*CertEntry
+}
+
+type CertEntry struct {
+	Domain    string
+	PubKey    []byte
+	CreatedAt int64
+	Revoked   bool
+	RevokedAt int64
+}
+
+var _ abcitypes.Application = (*PKIApplication)(nil)
+
+func NewPKIApplication() *PKIApplication {
+	return &PKIApplication{&PKIState{make(map[string]*CertEntry)}, merkle.NewTree([][]byte{}), []string{}}
+}
+
+func (app *PKIApplication) Info(_ context.Context, info *abcitypes.RequestInfo) (*abcitypes.ResponseInfo, error) {
+	return &abcitypes.ResponseInfo{}, nil
+}
+
+func (app *PKIApplication) Query(_ context.Context, req *abcitypes.RequestQuery) (*abcitypes.ResponseQuery, error) {
+	domain := string(req.Data)
+	i, exists := slices.BinarySearch(app.sortedDomains, domain)
+	if !exists {
+		return &abcitypes.ResponseQuery{Code: 1, Log: "domain not found"}, nil
+	}
+
+	proof, err := app.tree.Proof(i)
+	if err != nil {
+		return &abcitypes.ResponseQuery{Code: 2, Log: "error creating proof"}, nil
+	}
+	certEntry := app.pkiState.Certs[domain]
+
+	responseJson, err := json.Marshal(struct {
+		CertEntry *CertEntry
+		Proof     []merkle.ProofElement
+		RootHash  []byte
+	}{certEntry, proof, app.tree.RootHash()})
+	if err != nil {
+		return &abcitypes.ResponseQuery{Code: 3, Log: "error marshaling cert entry"}, nil
+	}
+	return &abcitypes.ResponseQuery{Code: 0, Value: responseJson}, nil
+}
+
+func (app *PKIApplication) CheckTx(_ context.Context, check *abcitypes.RequestCheckTx) (*abcitypes.ResponseCheckTx, error) {
+	code := app.isTxValid(check.Tx)
+	return &abcitypes.ResponseCheckTx{Code: code}, nil
+}
+
+func (app *PKIApplication) signatureValid(pubKeyBytes, message, signature []byte) bool {
+	if len(pubKeyBytes) != ed25519.PublicKeySize {
+		return false
+	}
+
+	if len(signature) != ed25519.SignatureSize {
+		return false
+	}
+
+	return ed25519.Verify(ed25519.PublicKey(pubKeyBytes), message, signature)
+}
+
+func (app *PKIApplication) isTxValid(tx []byte) uint32 {
+	parts := bytes.Split(tx, []byte("|"))
+	if len(parts) < 2 {
+		return 1
+	}
+
+	command := string(parts[0])
+	switch command {
+	case "REGISTER":
+		if len(parts) != 4 {
+			return 1
+		}
+
+		domain, pubKeyHex, signHex := parts[1], parts[2], parts[3]
+
+		_, exist := app.pkiState.Certs[string(domain)]
+		if exist {
+			return 1
+		}
+
+		pubKey, err := hex.DecodeString(string(pubKeyHex))
+		if err != nil {
+			return 1
+		}
+
+		signature, err := hex.DecodeString(string(signHex))
+		if err != nil {
+			return 1
+		}
+
+		if !app.signatureValid(pubKey, domain, signature) {
+			return 1
+		}
+
+	case "REVOKE":
+		if len(parts) != 3 {
+			return 1
+		}
+
+		domain, signHex := parts[1], parts[2]
+
+		certEntry, exist := app.pkiState.Certs[string(domain)]
+		if !exist {
+			return 1
+		}
+
+		signature, err := hex.DecodeString(string(signHex))
+		if err != nil {
+			return 1
+		}
+
+		if certEntry.Revoked {
+			return 1
+		}
+
+		if !app.signatureValid(certEntry.PubKey, domain, signature) {
+			return 1
+		}
+	default:
+		return 1
+	}
+
+	return 0
+}
+
+func (app *PKIApplication) InitChain(_ context.Context, chain *abcitypes.RequestInitChain) (*abcitypes.ResponseInitChain, error) {
+	return &abcitypes.ResponseInitChain{}, nil
+}
+
+func (app *PKIApplication) PrepareProposal(_ context.Context, proposal *abcitypes.RequestPrepareProposal) (*abcitypes.ResponsePrepareProposal, error) {
+	return &abcitypes.ResponsePrepareProposal{Txs: proposal.Txs}, nil
+}
+
+func (app *PKIApplication) ProcessProposal(_ context.Context, proposal *abcitypes.RequestProcessProposal) (*abcitypes.ResponseProcessProposal, error) {
+	return &abcitypes.ResponseProcessProposal{Status: abcitypes.ResponseProcessProposal_ACCEPT}, nil
+}
+
+func (app *PKIApplication) FinalizeBlock(_ context.Context, req *abcitypes.RequestFinalizeBlock) (*abcitypes.ResponseFinalizeBlock, error) {
+	txs := make([]*abcitypes.ExecTxResult, len(req.Txs))
+
+	for i, tx := range req.Txs {
+		code := app.isTxValid(tx)
+		if code != 0 {
+			txs[i] = &abcitypes.ExecTxResult{Code: code}
+			continue
+		}
+		parts := bytes.Split(tx, []byte("|"))
+		command := string(parts[0])
+		domain := string(parts[1])
+
+		switch command {
+		case "REGISTER":
+			pubKeyHex := parts[2]
+			pubKey, _ := hex.DecodeString(string(pubKeyHex))
+
+			certEntry := &CertEntry{
+				Domain:    string(domain),
+				PubKey:    pubKey,
+				CreatedAt: req.Time.Unix(),
+			}
+			certs := app.pkiState.Certs
+			certs[domain] = certEntry
+
+		case "REVOKE":
+			app.pkiState.Certs[domain].Revoked = true
+			app.pkiState.Certs[domain].RevokedAt = req.Time.Unix()
+		}
+
+		txs[i] = &abcitypes.ExecTxResult{Code: 0}
+	}
+
+	return &abcitypes.ResponseFinalizeBlock{TxResults: txs}, nil
+}
+
+func (app *PKIApplication) Commit(_ context.Context, commit *abcitypes.RequestCommit) (*abcitypes.ResponseCommit, error) {
+	certs := app.pkiState.Certs
+	certEntires := make([]*CertEntry, 0, len(certs))
+	for _, certEntry := range certs {
+		certEntires = append(certEntires, certEntry)
+	}
+
+	slices.SortFunc(certEntires, func(a, b *CertEntry) int {
+		return strings.Compare(a.Domain, b.Domain)
+	})
+
+	sortedDomains := make([]string, len(certEntires))
+	certEntriesByte := make([][]byte, len(certEntires))
+	for i, certEntry := range certEntires {
+		sortedDomains[i] = certEntry.Domain
+		certEntryBytes, err := json.Marshal(certEntry)
+		if err != nil {
+			return nil, fmt.Errorf("error marshaling cert entry: %w", err)
+		}
+		certEntriesByte[i] = certEntryBytes
+	}
+
+	app.sortedDomains = sortedDomains
+	app.tree = merkle.NewTree(certEntriesByte)
+
+	return &abcitypes.ResponseCommit{}, nil
+}
+
+func (app *PKIApplication) ListSnapshots(_ context.Context, snapshots *abcitypes.RequestListSnapshots) (*abcitypes.ResponseListSnapshots, error) {
+	return &abcitypes.ResponseListSnapshots{}, nil
+}
+
+func (app *PKIApplication) OfferSnapshot(_ context.Context, snapshot *abcitypes.RequestOfferSnapshot) (*abcitypes.ResponseOfferSnapshot, error) {
+	return &abcitypes.ResponseOfferSnapshot{}, nil
+}
+
+func (app *PKIApplication) LoadSnapshotChunk(_ context.Context, chunk *abcitypes.RequestLoadSnapshotChunk) (*abcitypes.ResponseLoadSnapshotChunk, error) {
+	return &abcitypes.ResponseLoadSnapshotChunk{}, nil
+}
+
+func (app *PKIApplication) ApplySnapshotChunk(_ context.Context, chunk *abcitypes.RequestApplySnapshotChunk) (*abcitypes.ResponseApplySnapshotChunk, error) {
+	return &abcitypes.ResponseApplySnapshotChunk{Result: abcitypes.ResponseApplySnapshotChunk_ACCEPT}, nil
+}
+
+func (app *PKIApplication) ExtendVote(_ context.Context, extend *abcitypes.RequestExtendVote) (*abcitypes.ResponseExtendVote, error) {
+	return &abcitypes.ResponseExtendVote{}, nil
+}
+
+func (app *PKIApplication) VerifyVoteExtension(_ context.Context, verify *abcitypes.RequestVerifyVoteExtension) (*abcitypes.ResponseVerifyVoteExtension, error) {
+	return &abcitypes.ResponseVerifyVoteExtension{}, nil
+}
---
+++ playground/integration/cert-registration-with-merkle/go.mod
@@ -0,0 +1,76 @@
+module cert-registration-with-merkle
+
+go 1.25.1
+
+require (
+	github.com/cometbft/cometbft v0.38.0
+	github.com/spf13/viper v1.15.0
+	local/merkle-trees v0.0.0
+)
+
+replace local/merkle-trees => ../../merkle-trees
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.3.2 // indirect
+	github.com/cespare/xxhash v1.1.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cometbft/cometbft-db v0.7.0 // indirect
+	github.com/cosmos/gogoproto v1.7.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/dgraph-io/badger/v2 v2.2007.4 // indirect
+	github.com/dgraph-io/ristretto v0.1.1 // indirect
+	github.com/dgryski/go-farm v0.0.0-20200201041132-a6ae2369ad13 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-kit/kit v0.12.0 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/golang/glog v1.2.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/btree v1.1.2 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/orderedcode v0.0.1 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/jmhodges/levigo v1.0.0 // indirect
+	github.com/klauspost/compress v1.16.0 // indirect
+	github.com/lib/pq v1.10.7 // indirect
+	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/minio/highwayhash v1.0.2 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oasisprotocol/curve25519-voi v0.0.0-20220708102147-0a8a51822cae // indirect
+	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.14.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.8.0 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/rs/cors v1.8.3 // indirect
+	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/spf13/afero v1.9.3 // indirect
+	github.com/spf13/cast v1.5.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.11.1 // indirect
+	github.com/subosito/gotenv v1.4.2 // indirect
+	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
+	github.com/tecbot/gorocksdb v0.0.0-20191217155057-f0fad39f321c // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20241202173237-19429a94021a // indirect
+	google.golang.org/grpc v1.70.0 // indirect
+	google.golang.org/protobuf v1.36.4 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
